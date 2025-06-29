f5685e1ab3291ba1da2c1811a376027263ef5583
DanielyVasconcelos2024 <daniely.vasconcelos@academico.uncisal.edu.br>
2025-06-28 22:14:26 -0300

    feat: implementa novas anotações e exercicios sobre annotation e inteference(1 e 2)

---
+++ revisao-basico-ts/script-basico-ex01.ts
@@ -0,0 +1,6 @@
+// ERROS DE ANOTTAÇOES trims e toLowercase 
+//definir o tipo da variavel 
+function normalizarTexto(texto: string ){
+    return texto.trim().toLowerCase()
+}
+console.log(normalizarTexto('Daniely VASCONCELOS')); 
